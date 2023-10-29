82baeb49e116486287b9f7f546a3dc7b3f8a4188
andychoii <andychoi439@gmail.com>
2023-10-28 20:20:15 -0400

    UI change and Folders/Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,80 @@
-import 'package:flutter/material.dart'; // Essential Flutter widgets
-import 'widgets/home_page.dart'; // Importing our main screen
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:provider/provider.dart';
 
-// Entry point for the Flutter app
-void main() => runApp(InstagramCloneApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-const int _whitePrimaryValue = 0xFFFFFFFF;
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "",
+          appId: "",
+          messagingSenderId: "",
+          projectId: "",
+          storageBucket: ''),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
 
-final MaterialColor white = MaterialColor(
-  _whitePrimaryValue,
-  <int, Color>{
-    50: Color(_whitePrimaryValue),
-    100: Color(_whitePrimaryValue),
-    200: Color(_whitePrimaryValue),
-    300: Color(_whitePrimaryValue),
-    400: Color(_whitePrimaryValue),
-    500: Color(_whitePrimaryValue),
-    600: Color(_whitePrimaryValue),
-    700: Color(_whitePrimaryValue),
-    800: Color(_whitePrimaryValue),
-    900: Color(_whitePrimaryValue),
-  },
-);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// Root widget for the application
-class InstagramCloneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Instagram Clone', // Title used by the OS task switcher
-      theme: ThemeData( // Defining the app's visual theme
-        primarySwatch: white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const LoginScreen();
+          },
+        ),
       ),
-      home: HomePage(), // SettingHomepage as the main screen
     );
   }
 }
